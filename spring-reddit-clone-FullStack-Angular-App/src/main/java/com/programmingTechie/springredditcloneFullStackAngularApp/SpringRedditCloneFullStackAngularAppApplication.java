483ac99e1347b9d37c1d4fa1504c5be55e169563
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2024-04-14 11:17:49 +0530

    Add files via upload

---
+++ spring-reddit-clone-FullStack-Angular-App/src/main/java/com/programmingTechie/springredditcloneFullStackAngularApp/SpringRedditCloneFullStackAngularAppApplication.java
@@ -0,0 +1,13 @@
+package com.programmingTechie.springredditcloneFullStackAngularApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditCloneFullStackAngularAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditCloneFullStackAngularAppApplication.class, args);
+	}
+
+}
