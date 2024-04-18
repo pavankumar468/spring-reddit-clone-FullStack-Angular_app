86306437fbf64719f1a90b4e6da1b8fd6f35c715
Pavan Kumar <pavankumarjan8@gmail.com>
2024-04-18 10:08:38 +0530

    created model classes

---
+++ spring-reddit-clone-FullStack-Angular-App/src/main/java/com/programmingTechie/springredditcloneFullStackAngularApp/repository/SubredditRepository.java
@@ -0,0 +1,11 @@
+package com.programmingTechie.springredditcloneFullStackAngularApp.repository;
+
+import com.programmingTechie.springredditcloneFullStackAngularApp.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
+
+
+}
